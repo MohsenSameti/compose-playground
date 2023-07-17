0ffab306ab7cd0cd20ac8e75bdf9c9d9b8ebbc02
Mohsen Sameti <s.mn.sameti@gmail.com>
2023-07-17 21:42:22 +0330

    step 11 done

--- app/src/main/java/com/samentic/hellocompose/MainActivity.kt
+++ app/src/main/java/com/samentic/hellocompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.samentic.hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -77,7 +78,7 @@ fun Greetings(
 @Composable
 fun Greeting(name: String) {
     var isExpanded by remember { mutableStateOf(false) }
-    val extraPadding = if (isExpanded) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(if (isExpanded) 48.dp else 0.dp)
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
