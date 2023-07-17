68576ea0e47f11f3e1c3cf88e9eb73c528ab2973
Mohsen Sameti <s.mn.sameti@gmail.com>
2023-07-17 21:27:42 +0330

    step 9 done

--- app/src/main/java/com/samentic/hellocompose/MainActivity.kt
+++ app/src/main/java/com/samentic/hellocompose/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.samentic.hellocompose
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,6 +9,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
@@ -24,7 +25,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.samentic.hellocompose.ui.theme.HelloComposeTheme
 
@@ -63,10 +63,11 @@ fun MyApp(
 @Composable
 fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = List(1000) { "$it"}
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        names.forEach { name ->
+    
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(names) {name -> 
             Greeting(name = name)
         }
     }
