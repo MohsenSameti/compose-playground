d8042acfe05b67d559a3bc06d4e23be4b599d066
Mohsen Sameti <s.mn.sameti@gmail.com>
2023-07-21 12:26:37 +0330

    finished step 6 state driven ui

--- app/src/main/java/com/samentic/hellocompose/WaterCounter.kt
+++ app/src/main/java/com/samentic/hellocompose/WaterCounter.kt
@@ -17,8 +17,14 @@ import androidx.compose.ui.unit.dp
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-        Text(text = "You've had $count glasses!")
-        Button(onClick = { count++ }) {
+        if (count > 0) {
+            Text(text = "You've had $count glasses!")
+        }
+        Button(
+            onClick = { count++ },
+            modifier = modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
             Text("Add one")
         }
     }
