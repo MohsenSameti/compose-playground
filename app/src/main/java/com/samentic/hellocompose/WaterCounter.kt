6aca5512021b8d1114ca66dfd037fd6d44aaf477
Mohsen Sameti <s.mn.sameti@gmail.com>
2023-07-22 06:10:24 +0330

    finished step 9 state hoisting

--- app/src/main/java/com/samentic/hellocompose/WaterCounter.kt
+++ app/src/main/java/com/samentic/hellocompose/WaterCounter.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -14,13 +15,31 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
+    StatefulCounter(modifier = modifier)
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by remember { mutableStateOf(0) }
+    StatelessCounter(
+        count = count,
+        onIncrement = { count++ },
+        modifier = modifier
+    )
+}
+
+@Composable
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by rememberSaveable  { mutableStateOf(0) }
         if (count > 0) {
             Text(text = "You've had $count glasses!")
         }
         Button(
-            onClick = { count++ },
+            onClick = onIncrement,
             modifier = modifier.padding(top = 8.dp),
             enabled = count < 10
         ) {
