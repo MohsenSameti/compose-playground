dc34f67a972babe5770b54627ef22cfd57459629
Mohsen Sameti <s.mn.sameti@gmail.com>
2023-08-16 08:44:34 +0330

    span style

--- app/src/main/java/com/samentic/hellocompose/MainActivity.kt
+++ app/src/main/java/com/samentic/hellocompose/MainActivity.kt
@@ -3,13 +3,69 @@ package com.samentic.hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.samentic.hellocompose.ui.theme.HelloComposeTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            HelloComposeTheme {
+                MainScreen()
+            }
         }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Column {
+        SpanString()
+    }
+}
+
+@Composable
+fun SpanString() {
+    Text(
+        buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(fontWeight = FontWeight.Bold, fontSize = 30.sp)
+            ) {
+                append("T")
+            }
+
+            withStyle(style = SpanStyle(color = Color.Gray)) {
+                append("his")
+            }
+            append(" is ")
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    color = Color.Blue
+                )
+            ) {
+                append("great!")
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    HelloComposeTheme {
+        MainScreen()
     }
 }
