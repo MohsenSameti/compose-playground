6bbf765c3afac2efdd52cff5a263777f582d3719
Mohsen Sameti <s.mn.sameti@gmail.com>
2023-08-30 01:07:45 +0330

    section 40.4

--- app/src/main/java/com/samentic/hellocompose/MainActivity.kt
+++ app/src/main/java/com/samentic/hellocompose/MainActivity.kt
@@ -1,26 +1,32 @@
+@file:Suppress("AnimateAsStateLabel")
+
 package com.samentic.hellocompose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.isActive
-import kotlin.random.Random
+import com.samentic.hellocompose.ui.theme.HelloComposeTheme
 
 
 @Suppress("AnimateAsStateLabel")
@@ -28,40 +34,48 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var temperature by remember { mutableStateOf(80) }
-
-            LaunchedEffect(Unit) {
-                while (isActive) {
-                    temperature = Random(System.currentTimeMillis()).nextInt(0, 100)
-                    delay(2000)
-                }
+            HelloComposeTheme {
+                RotationDemo()
             }
-
-            val animatedColor: Color by animateColorAsState(
-                targetValue = if (temperature < 20) Color.Black
-                else if (temperature < 40) Color.Cyan
-                else if (temperature < 60) Color.Yellow
-                else if (temperature < 80) Color(255, 127, 39)
-                else Color.Red,
-                animationSpec = tween(durationMillis = 600)
-            )
-
-            Box(modifier = Modifier.fillMaxSize()) {
-                Box(
-                    modifier = Modifier
-                        .size(200.dp)
-                        .align(Alignment.Center)
-                        .background(animatedColor)
-//                        .background(
-//                            if (temperature < 20) Color.Black
-//                            else if (temperature < 40) Color.Cyan
-//                            else if (temperature < 60) Color.Yellow
-//                            else if (temperature < 80) Color(255, 127, 39)
-//                            else Color.Red
-//                        )
-                )
-            }
-
         }
+    }
+}
+
+@Composable
+fun RotationDemo() {
+    var rotated by remember { mutableStateOf(false) }
+
+    val angle by animateFloatAsState(
+        targetValue = if (rotated) 360f else 0f,
+        animationSpec = tween(2500, easing = LinearEasing)
+    )
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_wifi_protect),
+            contentDescription = "protect wifi",
+            modifier = Modifier
+                .rotate(angle)
+                .padding(10.dp)
+                .size(300.dp)
+        )
+
+        Button(
+            onClick = { rotated = !rotated },
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Text("Rotate Protect wifi")
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun RotateDemoPreview() {
+    HelloComposeTheme {
+        RotationDemo()
     }
 }
