33216e4f5998a7bda6ddf1d5d6e7404038b7dfb6
Mohsen Sameti <s.mn.sameti@gmail.com>
2023-09-13 00:56:13 +0330

    section 26.2

--- app/src/main/java/com/samentic/hellocompose/MainActivity.kt
+++ app/src/main/java/com/samentic/hellocompose/MainActivity.kt
@@ -3,6 +3,19 @@ package com.samentic.hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
@@ -12,4 +25,34 @@ class MainActivity : ComponentActivity() {
 
         }
     }
+}
+
+@Composable
+fun TextCell(text: String, modifier: Modifier = Modifier) {
+    Text(
+        text = text,
+        fontSize = 70.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .padding(4.dp)
+            .size(100.dp, 100.dp)
+            .border(width = 4.dp, color = Color.Red)
+            .then(modifier)
+    )
+}
+
+@Composable
+fun MainScreen() {
+    Row {
+        TextCell("1")
+        TextCell("2")
+        TextCell("3")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
 }
