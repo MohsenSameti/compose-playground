c36dd40ac356bd2aa48d43f86fe8ee1a2488f762
Mohsen Sameti <s.mn.sameti@gmail.com>
2023-09-19 01:28:36 +0330

    section 27.3

--- app/src/main/java/com/samentic/hellocompose/MainActivity.kt
+++ app/src/main/java/com/samentic/hellocompose/MainActivity.kt
@@ -3,6 +3,19 @@ package com.samentic.hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
@@ -12,4 +25,40 @@ class MainActivity : ComponentActivity() {
 
         }
     }
+}
+
+@Composable
+fun TextCell(
+    text: String,
+    modifier: Modifier = Modifier,
+    fontSize: Int = 150
+) {
+    Text(
+        text = text,
+        fontSize = fontSize.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .padding(8.dp)
+            .border(width = 4.dp, color = Color.Red)
+            .then(modifier)
+    )
+}
+
+@Composable
+fun MainScreen() {
+    Box {
+        val height = 200.dp
+        val width = 200.dp
+
+        TextCell("1", Modifier.size(width = width, height = height))
+        TextCell("2", Modifier.size(width = width, height = height))
+        TextCell("3", Modifier.size(width = width, height = height))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
 }
