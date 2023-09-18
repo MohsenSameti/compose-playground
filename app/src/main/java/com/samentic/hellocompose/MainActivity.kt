f0e302d60063564cf2fa16d56e56a7bbb06efef0
Mohsen Sameti <s.mn.sameti@gmail.com>
2023-09-19 01:56:01 +0330

    section 28.4

--- app/src/main/java/com/samentic/hellocompose/MainActivity.kt
+++ app/src/main/java/com/samentic/hellocompose/MainActivity.kt
@@ -3,6 +3,15 @@ package com.samentic.hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
@@ -12,4 +21,25 @@ class MainActivity : ComponentActivity() {
 
         }
     }
+}
+
+@Composable
+fun ColorBox(modifier: Modifier) {
+    Box(modifier = Modifier
+        .padding(1.dp)
+        .size(width = 50.dp, height = 10.dp)
+        .then(modifier))
+}
+
+@Composable
+fun MainScreen() {
+    Box(modifier = Modifier.size(120.dp, 80.dp)) {
+        ColorBox(modifier = Modifier.background(Color.Blue))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
 }
